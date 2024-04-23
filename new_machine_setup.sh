70cc8781d2fb764cbcf9a4140c62e15568c0ec4e
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-04-23 11:35:50 -0400

    update for bazzite

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -1,43 +1,42 @@
+# bazzite/ublueOS
 
-flatpak remote-delete flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+ujust install-brew
 
-flatpak install flathub me.kozec.syncthingtk -y --user
+brew install bat
+brew install btop
+brew install tldr
+
+# setup atuin
+brew install atuin
+curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
+echo '[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh' >> ~/.bashrc
+echo 'eval "$(atuin init bash)"' >> ~/.bashrc
+
+flatpak install flathub com.github.zocker_160.SyncThingy -y --user
 flatpak install flathub com.calibre_ebook.calibre -y --user
 flatpak install flathub org.videolan.VLC -y --user
 flatpak install flathub com.slack.Slack -y --user
 flatpak install flathub com.discordapp.Discord -y --user
 flatpak install flathub com.github.tchx84.Flatseal -y --user
-flatpak install flathub com.github.Eloston.UngoogledChromium -y --user
 flatpak install flathub com.usebottles.bottles -y --user
 flatpak install flathub net.davidotek.pupgui2 -y --user
-flatpak install flathub com.github.johnfactotum.Foliate -y --user
-flatpak install flathub info.febvre.Komikku -y --user
 flatpak install flathub com.github.tenderowl.frog -y --user
 flatpak install flathub io.github.peazip.PeaZip -y --user
 flatpak install flathub net.cozic.joplin_desktop -y --user
 flatpak install flathub it.mijorus.smile -y --user
 flatpak install flathub org.signal.Signal -y --user
 flatpak install flathub org.onlyoffice.desktopeditors -y --user
-flatpak install flathub io.github.cboxdoerfer.FSearch -y --user
 flatpak install flathub fr.romainvigier.MetadataCleaner -y --user
 flatpak install flathub net.ankiweb.Anki -y --user
-flatpak install flathub com.microsoft.Edge -y --user
-flatpak install flathub ua.org.brezblock.q4wine -y --user
+# flatpak install flathub ua.org.brezblock.q4wine -y --user
 flatpak install flathub com.sublimetext.three -y --user
-flatpak install flathub org.kde.gwenview -y --user
 flatpak install flathub com.heroicgameslauncher.hgl -y --user
 flatpak install flathub dev.lapce.lapce -y --user
 flatpak install flathub org.qbittorrent.qBittorrent -y --user
-flatpak install flathub com.authy.Authy -y --user
-flatpak install flathub com.github.tchx84.Flatseal -y --user
 flatpak install flathub com.system76.Popsicle -y --user
 flatpak install flathub com.ultimaker.cura -y --user
-flatpak install flathub io.github.cboxdoerfer.FSearch -y --user
 flatpak install flathub net.sapples.LiveCaptions -y --user
-flatpak install flathub org.ferdium.Ferdium -y --user
 flatpak install flathub org.kde.kdenlive -y --user
-flatpak install flathub org.videolan.VLC -y --user
 flatpak install flathub org.x.Warpinator -y --user
 flatpak install flathub com.tomjwatson.Emote -y --user
 flatpak install flathub org.mozilla.firefox -y --user
@@ -50,14 +49,14 @@ flatpak install flathub eu.scarpetta.PDFMixTool -y --user
 flatpak install flathub io.gitlab.adhami3310.Converter -y --user
 
 # gnome extension manager
-flatpak install flathub com.mattjakeman.ExtensionManager -y --user
+# flatpak install flathub com.mattjakeman.ExtensionManager -y --user
 
 # manage Appimages
-flatpak install flathub it.mijorus.gearlever -y --user
+# flatpak install flathub it.mijorus.gearlever -y --user
 
 # setup KDE dolphin w/ dark backgroud
-flatpak install flathub org.kde.dolphin -y --user
-cp ./kdeglobals ~/.config/kdeglobals
+# flatpak install flathub org.kde.dolphin -y --user
+# cp ./kdeglobals ~/.config/kdeglobals
 
 cat ./.bashrc >> ~/.bashrc
 cp ./.bash_dotfiles ~/.bash_dotfiles
@@ -67,11 +66,6 @@ mkdir ~/.fonts
 cp ./.fonts/*.ttf ~/.fonts
 
 echo "export CALIBRE_USE_DARK_PALETTE=1" >> ~/.bash_profile
-
-# sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-# sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-# sudo dnf -y install ffmpeg
 
 # enable touchscreen scrolling on firefox wayland
 echo export MOZ_ENABLE_WAYLAND=1 | sudo tee /etc/profile.d/use-moz-enable-wayland.sh
