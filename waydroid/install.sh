714b3faadb28f28d2003ab1ee0737d1440a53c68
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-06-29 22:02:27 -0400

    install waydroid full ui script bazzite

---
+++ waydroid/install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+cat <<EOF > "$HOME/.local/share/applications/waydroid-full-ui.desktop"
+[Desktop Entry]
+Type=Application
+Name=Waydroid Full UI
+Exec=waydroid-full-ui
+Categories=X-WayDroid-App;
+X-Purism-FormFactor=Workstation;Mobile;
+Icon=waydroid
+NoDisplay=false
+EOF
+
+cat <<EOF > "$HOME/.local/bin/waydroid-full-ui"
+#!/usr/bin/bash
+
+source /etc/default/waydroid-launcher
+
+# Kill any previous remnants
+if [ "$(systemctl is-active waydroid-container.service)" == 'active' ]; then
+	pkexec /usr/libexec/waydroid-container-stop
+fi
+
+# Launch Cage & Waydroid
+pkexec /usr/libexec/waydroid-container-start
+
+waydroid show-full-ui &
+
+sleep 5
+
+$HOME/.local/bin/clear-waydroid-desktop-entries
+EOF
+
+cat <<EOF > "$HOME/.local/bin/clear-waydroid-desktop-entries"
+#!/bin/bash
+
+rm $HOME/.local/share/applications/waydroid.*.desktop
+EOF
+
+chmod +x $HOME/.local/share/applications/waydroid-full-ui.desktop
+chmod +x $HOME/.local/bin/waydroid-full-ui
+chmod +x $HOME/.local/bin/clear-waydroid-desktop-entries
